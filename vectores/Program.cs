2518388f6a886d1867ab4bf8f0e3daee4fc24b0b
Carlosalbertomv <carlosvela802@gmail.com>
2024-03-08 21:11:43 -0400

    ··

--- vectores/Program.cs
+++ vectores/Program.cs
@@ -5,46 +5,22 @@ namespace HelloWorld
   class Program
   {
     static void Main(string[] args)
-    {
-      /*Console.WriteLine("ingrese primera nota");
-      double nota1 = Convert.ToDouble(Console.ReadLine());
-      
-      Console.WriteLine("ingrese segunda nota");
-      double nota2 = Convert.ToDouble(Console.ReadLine());
+    {   //vectores de numeros 
+        int[] numeros = new int[] {1,2,3,4,5};
+        //vectores de letras 
+        char[] letras = new char[] {'a','b','c','d','e'};
+        //vectores de palabras
+        string[] palabras = new string[] {"hola","insanos","hello","hola","mundo"};
 
-      double promedio = (nota1 + nota2) / 2;
-
-      Console.WriteLine($"El promedio de las 2 notas es: {promedio}");*/
-
-      //rotar();
-      rotar2(); 
-      Console.ReadLine();
-
+        //generar un vector vacio
+        /*int[] vacio = new int[10];
+        Console.WriteLine("\nVector vacio:");
+        imprimirVectorVacio(vacio);*/     
       
      
     }
 
-    static void rotar()
-    {
-      string numero = Console.ReadLine();
-      char ultimo = numero[numero.Length - 1];
-      string resto = numero.Substring(0, numero.Length - 1);
-      string resultado = ultimo + resto;
-      Console.WriteLine(resultado);
-    }
-
-    static void rotar2()
-    {
-      string numero = Console.ReadLine();
-      char primero = numero[0];
-      char segundo = numero[1];
-      string resto = numero.Substring(2, numero.Length - 2);
-      string resultado = resto + primero + segundo;
-      Console.WriteLine(resultado);
-
-      //vectores
-    
-    }
+   
  }
  
 }
