2582af77653f3599e560a50c8ad2f23360390dc0
Carlosalbertomv <carlosvela802@gmail.com>
2024-03-13 21:58:18 -0400

    nada

--- Matrices/Program.cs
+++ Matrices/Program.cs
@@ -41,13 +41,63 @@ namespace programa
             }
             ImprimirMatriz(numeros);*/
 
-            int [,] mat = LlenarMatrizB(4,4);
+            int [,] mat = LlenarMatrizC(4,4);
             ImprimirMatriz(mat);
         }
 
         /////////////NUMEROS////////////////////
         ///
         
+
+        static int[,] LlenarMatrizSerpienteB(int filas, int columnas)
+        {
+            int[,] matriz = new int[filas, columnas]; // Crear una matriz vacía con las dimensiones especificadas
+
+            int valor = 1;
+            for (int fila = 0; fila < filas; fila++)
+            {
+                // Determinar la dirección de llenado de la fila (izquierda a derecha o derecha a izquierda)
+                bool derecha = fila % 2 == 0;
+
+                // Llenar la fila en la dirección determinada
+                if (derecha)
+                {
+                    for (int columna = 0; columna < columnas; columna++)
+                    {
+                        matriz[fila, columna] = valor++;
+                    }
+                }
+                else
+                {
+                    for (int columna = columnas - 1; columna >= 0; columna--)
+                    {
+                        matriz[fila, columna] = valor++;
+                    }
+                }
+            }
+
+            return matriz;
+        }
+
+        static int[,] LlenarMatrizC(int filas, int columnas)
+        {
+            int[,] matriz = new int[filas, columnas]; // Crear una matriz vacía con las dimensiones especificadas
+            int contador = 1;
+
+            for (int f = filas -1 ; f >=0; f--)
+            {
+                for (int c = 0; c < columnas; c++)
+                {
+                    matriz[f, c] = contador;
+                    contador = contador + 1;
+                    
+                     // Llenar la matriz con números del 1 al número de columnas
+                }
+            }
+
+            return matriz;
+        }
+
         static int[,] LlenarMatrizB(int filas, int columnas)
         {
             int[,] matriz = new int[filas, columnas]; // Crear una matriz vacía con las dimensiones especificadas
