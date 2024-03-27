b12eb5633bd4cfe5ecd7d3997092c57a1eabb929
Carlosalbertomv <carlosvela802@gmail.com>
2024-03-26 21:54:00 -0400

    txt

---
+++ ArchivosTxt/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HelloWorld
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string ruta = @"E:/UPC/carlos/Carlos_proIII/ArchivosTxt/CarlosAlberto.txt";
+      
+      FileStream archivo = null;
+      archivo = File.Open(ruta, FileMode.OpenOrCreate);
+    }
+
+    
+ }
+ 
+}
